3c939ac774f7716fd26ba7696c3419c327904bff
Anna <annaosipchik03@gmail.com>
2021-09-04 23:28:24 +0300

    сделала 1 лабу

---
+++ lesson_1/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner answ = new Scanner(System.in);
+        System.out.println("Введите ваш х от -20 до 20: ");
+        double num = answ.nextDouble();
+        System.out.println("Вводите степень (>0): ");
+        int k = answ.nextInt();
+        double sum = Teylor(k, num);
+        BigDecimal s = Teylor_big(k,BigDecimal.valueOf(num));
+        System.out.println("Ваша сумма в примитивных типах данных: " + sum);
+        System.out.println("Ваша сумма в BigDecimal: " + s);
+    }
+
+    public static double Teylor(int  k, double x)
+    {
+        int n = 1;
+        double sum = x;
+        double tmp = x;
+        double e = Math.pow(10, -k);
+        while (Math.abs(tmp) > e)
+        {
+            tmp*=x*x/(2*n+1)/(2*n)*(-1);
+            sum += tmp;
+            n+=1;
+        }
+        return sum;
+    }
+    public static BigDecimal Teylor_big(int k, BigDecimal x)
+    {
+        int n = 1;
+        BigDecimal tmp;
+        BigDecimal sum;
+        sum = x;
+        tmp = x;
+        MathContext mc = new MathContext(10);
+        BigDecimal e = BigDecimal.TEN.pow(-k, mc);
+        while (tmp.abs().compareTo(e) > 0)
+        {
+            tmp = tmp.multiply(x).multiply(x);
+            tmp = tmp.divide((BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(n))), mc);
+            tmp = tmp.divide((BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(n)).add(BigDecimal.ONE)), mc);
+            tmp = tmp.multiply(BigDecimal.valueOf(-1));
+            sum = sum.add(tmp);
+            n+=1;
+        }
+        return sum;
+    }
+}
+
