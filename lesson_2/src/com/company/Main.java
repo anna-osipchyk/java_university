eaaf1938c757925838500bc5e0a94d93bc7bc47b
Anna <annaosipchik03@gmail.com>
2021-09-16 00:26:49 +0300

    делаю 1 задачу лабы 2

--- lesson_2/src/com/company/Main.java
+++ lesson_2/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("d");
+    Matrix_1 m = new Matrix_1();
+    m.generating();
+    m.printing();
+    ArrayList<Integer> c = m.monotone();
+    for(Integer i: c)
+    {
+        System.out.println(i);
     }
+    }
+
 }
