72f1dad67286646fd2a0123364bbb7d09b6190f6
Anna <annaosipchik03@gmail.com>
2021-09-16 15:31:22 +0300

    сделала 2 задачи лабы 2

--- lesson_2/src/com/company/Main.java
+++ lesson_2/src/com/company/Main.java
@@ -1,18 +1,37 @@
 package com.company;
+// Задача 1
+//Выведите  номера столбцов, элементы каждой
+// из которых образуют монотонную последовательность
+// (монотонно убывающую или монотонно возрастающую).
 
+// Задача 2
+//Подсчитать количество строк заданной матрицы, являющихся перестановкой чисел
+// -1, -2, -3
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-    Matrix_1 m = new Matrix_1();
-    m.generating();
-    m.printing();
-    ArrayList<Integer> c = m.monotone();
-    for(Integer i: c)
+        task_1();
+        task_2();
+    }
+    public static void task_1()
     {
-        System.out.println(i);
+        Matrix_1 m = new Matrix_1();
+        m.generating();
+        m.printing();
+        ArrayList<Integer> c = m.monotone();
+        System.out.println("The numbers of rows that are momotone: ");
+        for(Integer i: c)
+        {
+        System.out.print(i+" ");
+        }
     }
+    public static void task_2() {
+        Matrix_2 m_2 = new Matrix_2();
+        m_2.input();
+        m_2.printing();
+        System.out.println("The numbers of lines that are permutations: ");
+        System.out.println(m_2.permutations());
     }
-
 }
---
+++ lesson_2/src/com/company/Matrix_2.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Matrix_2 {
+    private final int dim = 3;
+    private int [][] matrix;
+    Pattern pattern = Pattern.compile("\\[-[1-3], -[1-3], -[1-3]]");
+    public void input() {
+        matrix = new int[dim][dim];
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Be ready to input the elements of matrix...");
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                System.out.println("Enter the element: ");
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+    }
+        public ArrayList<Integer> permutations()
+    {
+        ArrayList<Integer> list_of_perms = new ArrayList<>();
+        String string;
+
+        for(int i=0;i< dim;i++)
+        {
+            string = Arrays.toString(matrix[i]);
+            Matcher matcher = pattern.matcher(string);
+            boolean found = matcher.matches();
+            if (found) {
+                list_of_perms.add(i);
+
+            }
+        }
+    return list_of_perms;
+    }
+    public void printing() {
+        System.out.println("Your matrix: ");
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
