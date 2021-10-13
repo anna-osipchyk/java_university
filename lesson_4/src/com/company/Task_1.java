355d59cfff9a42d7918edb93b16366664b1eac55
Anna <annaosipchik03@gmail.com>
2021-10-14 01:44:23 +0300

    сделала часть лабы 4

--- lesson_4/src/com/company/Task_1.java
+++ lesson_4/src/com/company/Task_1.java
@@ -5,37 +5,6 @@ import java.io.*;
 import java.util.stream.Collectors;
 
 public class Task_1 {
-//    private static Pattern pattern_1;
-//    private static Matcher matcher_1;
-//    public static void solve() throws Exception
-//    {
-//        FileReader f = new FileReader("input.txt");
-//        Scanner sc = new Scanner(f);
-//        String s;
-//        List<String> list = new ArrayList<String>();
-//
-//        List<String> list_1 = new ArrayList<String>();
-//        while(sc.hasNextLine())
-//        {
-//            s = sc.nextLine();
-//            pattern_1 = Pattern.compile("\\(.+?\\)");
-//            Pattern pattern_2 = Pattern.compile("\\(.+?\\(");
-//            Pattern pattern_3 = Pattern.compile("\\).+?\\)");
-//
-//            //String[] tmp_arr_of_str = pattern_1.split(new String(s));
-//            String[] tmp_arr_of_str_2 = pattern_2.split(s);
-//            String[] tmp_arr_of_str_3 = pattern_3.split(Arrays.toString(tmp_arr_of_str_2));
-//            list_1.addAll(Arrays.asList(tmp_arr_of_str_3));
-//
-//            list.addAll(Arrays.asList(tmp_arr_of_str_2));
-//
-//        }
-//    for(String string: list_1 )
-//    {
-//        System.out.print(string);
-//    }
-//
-//    }
     public static void solv() throws Exception
     {
         FileReader f = new FileReader("input.txt");
@@ -59,10 +28,11 @@ public class Task_1 {
         for(int i = 0; i< ch_arr.length; i++) {
             if (!flag && ch_arr[i]=='(')
             {
+                ch_arr[i]='0';
                 counter_of_right_brackets++;
                 flag = true;
                 i++;
-                while(ch_arr[i]!=')' && ch_arr[i]!='\n')
+                while(ch_arr[i]!=')')
                 {
                     if(ch_arr[i]=='(')
                     {
@@ -89,6 +59,10 @@ public class Task_1 {
                 {
                     counter_of_left_brackets+=1;
                 }
+                if (counter_of_left_brackets==counter_of_right_brackets&& ch_arr[i]==')')
+                {
+                    ch_arr[i]='0';
+                }
 
             }
 
@@ -97,28 +71,11 @@ public class Task_1 {
                 chars.add(c);
             }
         }
+        chars.removeIf(ch-> ch.equals('0'));
         String listString = chars.stream().map(Object::toString)
                 .collect(Collectors.joining(""));
         System.out.println(listString);
-        //System.out.println(chars.toString());
         }
-//    public static void solution() throws Exception {
-//
-//        FileReader f = new FileReader("input.txt");
-//        Scanner sc = new Scanner(f);
-//        ArrayList<String> arr_of_s = new ArrayList<>();
-//        String tmp;
-//
-//        while (sc.hasNextLine()) {
-//            tmp = sc.nextLine();
-//            String[] tmp_arr_of_str_2 = tmp.split(" ");
-//            arr_of_s.addAll(Arrays.asList(tmp_arr_of_str_2));
-//        }
-//        for (String str: arr_of_s)
-//        {
-//            System.out.println(str);
-//        }
-//    }
 
 
 }
